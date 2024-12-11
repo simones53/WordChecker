5803dab5fef9d7c2640978e0348527e79efc9bc4
simones53 <simone.sevita08@gmail.com>
2024-12-11 17:01:39 +0000

    doenee

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
-        public static void main(String[] args)
+    public static void main(String[] args)
     {
         WordChecker w = new WordChecker();
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+        System.out.println(words);
+
+        WordChecker x = new WordChecker(words);
+
+        System.out.println(x.isWordChain());
     }
 
 }
--- WordChecker.java
+++ WordChecker.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
 public class WordChecker {
-    /** Initialized in the constructor and contains no null elements */
+
+    private ArrayList<String> wordList;
+
     public WordChecker(ArrayList<String> list)
     {
         wordList = list;
     }
 
-    private ArrayList<String> wordList;
-
+    public WordChecker() 
+    {
+        wordList = new ArrayList<String>();
+    }
+    /** Initialized in the constructor and contains no null elements */
 
     /**
      * Returns true if each element of wordList (except the first) contains the
@@ -17,7 +22,13 @@ public class WordChecker {
      * Postcondition: wordList is unchanged.
      */
     public boolean isWordChain() {
-        /* to be implemented in part (a) */ }
+        for(int i = 1; i < wordList.size(); i++) {
+            String after = wordList.get(i);
+            String before = wordList.get(i - 1);
+            if(after.indexOf(before) < 0) return false;
+        }
+        return true;
+    }
 
     /**
      * Returns an ArrayList<String> based on strings from wordList that start
@@ -29,5 +40,12 @@ public class WordChecker {
      * wordList.
      */
     public ArrayList<String> createList(String target) {
-        /* to be implemented in part (b) */ }
+        ArrayList<String> list = new ArrayList<String>();
+        for (String s : wordList) {
+            if(s.indexOf(target) == 0) {
+                list.add(s.substring(target.length()));
+            }
+        }
+        return list;
+    }
 }
